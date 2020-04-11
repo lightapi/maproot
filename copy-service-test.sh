76575637eefdc64e6baf75f0fe03b5b5a3228bf0
Steve Hu <stevehu@gmail.com>
2020-04-10 23:56:33 -0400

    fixes #12 add copy-service-test.sh for test cloud deployment of service jars

---
+++ copy-service-test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+scp covid-query/target/covid-query-0.1.0.jar steve@test1:/home/steve/networknt/light-config-test/light-portal/test-cloud/hybrid-query/service
+scp covid-query/target/covid-query-0.1.0.jar steve@test2:/home/steve/networknt/light-config-test/light-portal/test-cloud/hybrid-query/service
+scp covid-query/target/covid-query-0.1.0.jar steve@test3:/home/steve/networknt/light-config-test/light-portal/test-cloud/hybrid-query/service
+scp covid-command/target/covid-command-0.1.0.jar steve@portal:/home/steve/networknt/light-config-test/light-portal/test-cloud/hybrid-command/service
