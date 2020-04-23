a632265e87e2b50ceb1e442571e3a055c48cbf59
Steve Hu <stevehu@gmail.com>
2020-04-23 11:18:07 -0400

    fixes #22 test cloud deployment script to move to light-chain light-config-test

--- copy-service-test.sh
+++ copy-service-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-scp covid-query/target/covid-query-0.1.0.jar steve@test1:/home/steve/networknt/light-config-test/light-portal/test-cloud/hybrid-query/service
-scp covid-query/target/covid-query-0.1.0.jar steve@test2:/home/steve/networknt/light-config-test/light-portal/test-cloud/hybrid-query/service
-scp covid-query/target/covid-query-0.1.0.jar steve@test3:/home/steve/networknt/light-config-test/light-portal/test-cloud/hybrid-query/service
-scp covid-command/target/covid-command-0.1.0.jar steve@test3:/home/steve/networknt/light-config-test/light-portal/test-cloud/hybrid-command/service
+scp covid-query/target/covid-query-0.1.0.jar steve@test1:/home/steve/light-chain/light-config-test/light-portal/test-direct/hybrid-query/service
+scp covid-query/target/covid-query-0.1.0.jar steve@test2:/home/steve/light-chain/light-config-test/light-portal/test-direct/hybrid-query/service
+scp covid-query/target/covid-query-0.1.0.jar steve@test3:/home/steve/light-chain/light-config-test/light-portal/test-direct/hybrid-query/service
+scp covid-command/target/covid-command-0.1.0.jar steve@test3:/home/steve/light-chain/light-config-test/light-portal/test-direct/hybrid-command/service
--- view/deploy-test.sh
+++ view/deploy-test.sh
@@ -2,5 +2,5 @@
 echo "Build the view in in test mode"
 yarn build-dev
 echo "Build completed in build folder, start copying to remote test2 server"
-scp -r ./build/* steve@test2:/home/steve/networknt/light-config-test/light-router/test-portal/lightapi/build
+scp -r ./build steve@test2:/home/steve/light-chain/light-config-test/light-router/test-direct/lightapi
 echo "Copied!"
