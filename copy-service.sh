954cc5dbc5ea9391f8fe99dd0418e3675aefb5ce
Steve Hu <stevehu@gmail.com>
2020-05-06 22:31:54 -0400

    fixes #32 upgrade to Kafka 2.5.0 and Confluent 5.3.3

--- copy-service.sh
+++ copy-service.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp covid-command/target/covid-command-0.1.0.jar ../light-config-test/light-portal/local-consul/hybrid-command/service
-cp covid-query/target/covid-query-0.1.0.jar ../light-config-test/light-portal/local-consul/hybrid-query/service
+cp covid-command/target/covid-command-0.1.0.jar /home/steve/light-chain/light-config-test/light-portal/local-direct/hybrid-command/service
+cp covid-query/target/covid-query-0.1.0.jar /home/steve/light-chain/light-config-test/light-portal/local-direct/hybrid-query/service
