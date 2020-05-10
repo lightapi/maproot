928f8f0e38b77b5c7f0e7cf0bf5daf955f8305a1
Steve Hu <stevehu@gmail.com>
2020-05-10 08:33:33 -0400

    fixes #46 render private message in a table with reply and delete icons

--- view/src/components/Notification/Messages.js
+++ view/src/components/Notification/Messages.js
@@ -1,13 +1,71 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import DeleteIcon from '@material-ui/icons/Delete';
+import ReplyIcon from '@material-ui/icons/Reply'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { timeConversion } from '../../Utils';
+
+const useRowStyles = makeStyles({
+    root: {
+      '& > *': {
+        borderBottom: 'unset',
+      },
+    },
+  });
+
+  function Row(props) {
+    const { row } = props;
+    const classes = useRowStyles();
+  
+    return (
+      <React.Fragment>
+        <TableRow className={classes.root}>
+          <TableCell component="th" scope="row">{timeConversion((new Date()).getTime() - row.timestamp)}</TableCell>
+          <TableCell align="left"><ReplyIcon onClick={() => console.log("reply")}/>{row.fromId}</TableCell>
+          <TableCell align="left">{row.subject}</TableCell>
+          <TableCell align="right">
+              <DeleteIcon onClick={() => console.log("delete is clicked", row.timestamp, row.fromId)} />
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={4}>
+            {row.content}
+          </TableCell>
+        </TableRow>
+      </React.Fragment>
+    );
+  }
 
 export default function Messages(props) {
 	console.log("props = ", props);
 	console.log("data = ", props.location.state.data);
-	const data = props.location.state.data;
+	const messages = props.location.state.data;
     return (
         <div>
             <h2>Private Messages</h2>
-			<pre>{ JSON.stringify(data, null, 2) }</pre>
+            <TableContainer component={Paper}>
+                <Table aria-label="collapsible table">
+                    <TableHead>
+                    <TableRow>
+                        <TableCell>Time</TableCell>
+                        <TableCell align="left">From</TableCell>
+                        <TableCell align="left">Subject</TableCell>
+                        <TableCell/>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {messages.map((msg, index) => (
+                        <Row key={index} row={msg} />
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </div>
     );
 }
