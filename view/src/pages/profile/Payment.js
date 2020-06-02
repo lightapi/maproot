be4fe7e9f62c5c2ddfc71916d28930c062a5a381
Steve Hu <stevehu@gmail.com>
2020-06-02 15:20:14 -0400

    fixes #85 update payment.js to ensure that form model is populated correctly

--- view/src/pages/profile/Payment.js
+++ view/src/pages/profile/Payment.js
@@ -30,7 +30,7 @@ export default function Payment(props) {
   };
 
   const updatePayment = () => {
-    props.history.push({pathname: '/app/form/updatePayment', state: { data }});
+    props.history.push({pathname: '/app/form/updatePayment', state: { data: {email, payments: data }}});
   };
 
   let wait;
