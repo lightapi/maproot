66cc414244c4b18fd316f5d34f202151131ade24
Steve Hu <stevehu@gmail.com>
2020-06-04 06:51:50 -0400

    fixes #87 add buttons to the home page for website

--- view/src/pages/sites/Home.js
+++ view/src/pages/sites/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import Fab from '@material-ui/core/Fab';
 import { makeStyles } from '@material-ui/styles';
+import { useSiteDispatch } from "../../context/SiteContext";
 
 /*
 For independent compnent, use the makeStyles and have a chance to override the style using data from the
@@ -46,11 +48,24 @@ const useStyles = makeStyles({
 export default function Home(props) {
     const styleProps = { nameSize: props.site.home.nameSize || '110px', titleSize: props.site.home.titleSize || '70px' }
     var classes = useStyles(styleProps);
+    var siteDispatch = useSiteDispatch();
+
+    const onButtonClick = (menu) => {
+      siteDispatch({ type: "UPDATE_MENU", menu }); 
+    }
+
     return (
         <div className={classes.home} style ={{ backgroundImage: `url(${props.site.home.background})`, backgroundSize: 'cover',  overflow: 'hidden' }}>
             <div className={classes.homeContent}>
                 <div className={classes.homeName}>{props.site.home.name}</div>
                 <div className={classes.homeTitle}>{props.site.home.title}</div>
+                <div>
+                {props.site.home.buttons.map(button => (
+                  <Fab variant="extended" color="primary" onClick={() => onButtonClick(button.menu)}>
+                   {button.label}
+                 </Fab>
+                ))}
+                </div>
             </div>
         </div>
     );
