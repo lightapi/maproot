d653952e0a03b054a40b54aba9b1d5ef584682d5
Steve Hu <stevehu@gmail.com>
2020-06-08 22:26:53 -0400

    fixes #92 view add pagination for customer and merchant orders

--- view/src/pages/profile/MerchantOrderTab.js
+++ view/src/pages/profile/MerchantOrderTab.js
@@ -1,34 +1,70 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import { useApiGet } from '../../hooks/useApiGet';
+import TablePagination from '@material-ui/core/TablePagination';  
 import { useUserState } from "../../context/UserContext";
 import MerchantOrderList from "./MerchantOrderList";
+import Cookies from 'universal-cookie'
 
 export default function MerchantOrdersTab(props) {
-    const [offset, setOffset] = useState(0);
-    const [limit, setLimit] = useState(25);
-    const [orders, setOrders] = useState();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState();
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(25);
+    const [count, setCount] = useState(0);
+    const [orders, setOrders] = useState([]);
     const { email } = useUserState();
     const { status } = props;
+
     const cmd = {
         host: 'lightapi.net',
         service: 'user',
         action: 'getMerchantOrder',
         version: '0.1.0',
-        data: { email, offset, limit, status }
+        data: { email, offset: page * rowsPerPage, limit: rowsPerPage, status }
     }
     console.log("cmd = ", cmd);
     const url = '/portal/query?cmd=' + encodeURIComponent(JSON.stringify(cmd));
     const headers = {};
       
-    const callback = (data) => {
-        console.log("data = ", data);
-        setOrders(data);
-    }
-    const { isLoading, data, error } = useApiGet({url, headers, callback});
-    
+    const queryOrders = async (url, headers) => {
+        try {
+          setLoading(true);
+          const response = await fetch(url, { headers, credentials: 'include'});
+          if (!response.ok) {
+            const error = await response.json();
+            setError(error.description);
+            setOrders([]);
+          } else {
+            const data = await response.json();
+            setOrders(data.orders);
+            setCount(data.total)
+          }
+          setLoading(false);
+        } catch (e) {
+          console.log(e);
+          setError(e);
+          setOrders([]);
+          setLoading(false);
+        }
+      };
+  
+    useEffect(() => {
+        const cookies = new Cookies();
+        const headers = {'X-CSRF-TOKEN': cookies.get('csrf')};
+        queryOrders(url, headers);
+    }, [page, rowsPerPage]);
+      
+    const handleChangePage = (event, newPage) => {  
+        setPage(newPage);  
+      };  
+  
+    const handleChangeRowsPerPage = event => {  
+        setRowsPerPage(+event.target.value);  
+        setPage(0);  
+    };      
+  
     let wait;
-    if(isLoading) {
+    if(loading) {
         wait = <div><CircularProgress/></div>;
     } else if(error) {
         wait = (
@@ -40,6 +76,15 @@ export default function MerchantOrdersTab(props) {
         wait = (
           <div>
             <MerchantOrderList {...props} orders={orders}/>
+            <TablePagination  
+              rowsPerPageOptions={[10, 25, 100]}  
+              component="div"  
+              count={count}  
+              rowsPerPage={rowsPerPage}  
+              page={page}  
+              onChangePage={handleChangePage}  
+              onChangeRowsPerPage={handleChangeRowsPerPage}  
+            />  
           </div>
         )
     }
--- view/src/pages/profile/UserOrders.js
+++ view/src/pages/profile/UserOrders.js
@@ -1,20 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import DeleteIcon from '@material-ui/icons/Delete';
-import ReplyIcon from '@material-ui/icons/Reply'
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import CircularProgress from '@material-ui/core/CircularProgress';
-
-import { timeConversion } from '../../Utils';
-import { useApiGet } from '../../hooks/useApiGet';
+import TablePagination from '@material-ui/core/TablePagination';  
 import { useUserState } from "../../context/UserContext";
 import UserOrderList from "./UserOrderList";
+import Cookies from 'universal-cookie'
 
 const useRowStyles = makeStyles({
     root: {
@@ -26,29 +16,64 @@ const useRowStyles = makeStyles({
 
 export default function UserOrders(props) {
     // load customerOrders from the API with email from UserContext. 
-    const [offset, setOffset] = useState(0);
-    const [limit, setLimit] = useState(25);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState();
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(25);
+    const [count, setCount] = useState(0);
     const [orders, setOrders] = useState([]);
     const { email } = useUserState();
+
     const cmd = {
-        host: 'lightapi.net',
-        service: 'user',
-        action: 'getCustomerOrder',
-        version: '0.1.0',
-        data: { email, offset, limit }
-    }
-    console.log("cmd = ", cmd);
+      host: 'lightapi.net',
+      service: 'user',
+      action: 'getCustomerOrder',
+      version: '0.1.0',
+      data: { email, offset: page * rowsPerPage, limit: rowsPerPage }
+    };
+
     const url = '/portal/query?cmd=' + encodeURIComponent(JSON.stringify(cmd));
     const headers = {};
-      
-    const callback = (data) => {
-        console.log("data = ", data);
-        setOrders(data);
-    }
-    const { isLoading, data, error } = useApiGet({url, headers, callback});
+
+    const queryOrders = async (url, headers) => {
+      try {
+        setLoading(true);
+        const response = await fetch(url, { headers, credentials: 'include'});
+        if (!response.ok) {
+          const error = await response.json();
+          setError(error.description);
+          setOrders([]);
+        } else {
+          const data = await response.json();
+          setOrders(data.orders);
+          setCount(data.total)
+        }
+        setLoading(false);
+      } catch (e) {
+        console.log(e);
+        setError(e);
+        setOrders([]);
+        setLoading(false);
+      }
+    };
+
+    useEffect(() => {
+      const cookies = new Cookies();
+      const headers = {'X-CSRF-TOKEN': cookies.get('csrf')};
+      queryOrders(url, headers);
+    }, [page, rowsPerPage]);
     
+    const handleChangePage = (event, newPage) => {  
+      setPage(newPage);  
+    };  
+
+    const handleChangeRowsPerPage = event => {  
+      setRowsPerPage(+event.target.value);  
+      setPage(0);  
+    };      
+
     let wait;
-    if(isLoading) {
+    if(loading) {
         wait = <div><CircularProgress/></div>;
     } else if(error) {
         wait = (
@@ -60,6 +85,15 @@ export default function UserOrders(props) {
         wait = (
           <div>
             <UserOrderList {...props} orders={orders}/>
+            <TablePagination  
+              rowsPerPageOptions={[10, 25, 100]}  
+              component="div"  
+              count={count}  
+              rowsPerPage={rowsPerPage}  
+              page={page}  
+              onChangePage={handleChangePage}  
+              onChangeRowsPerPage={handleChangeRowsPerPage}  
+            />  
           </div>
         )
     }
