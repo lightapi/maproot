e1ebab0c2d8e47a84a07e35c6b733525f61c35dc
Steve Hu <stevehu@gmail.com>
2020-06-17 22:46:26 -0400

    fixes #96 update copy-service-test.sh and view deploy-test.sh to test cloud

--- copy-service-test.sh
+++ copy-service-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-scp covid-query/target/covid-query-0.1.0.jar test1:/home/steve/light-chain/light-config-test/light-portal/test-direct/hybrid-query/service
-scp covid-query/target/covid-query-0.1.0.jar test2:/home/steve/light-chain/light-config-test/light-portal/test-direct/hybrid-query/service
-scp covid-query/target/covid-query-0.1.0.jar test3:/home/steve/light-chain/light-config-test/light-portal/test-direct/hybrid-query/service
-scp covid-command/target/covid-command-0.1.0.jar test3:/home/steve/light-chain/light-config-test/light-portal/test-direct/hybrid-command/service
+scp covid-query/target/covid-query-0.1.0.jar test1:/home/steve/light-chain/light-config-test/test1/hybrid-query/service
+scp covid-query/target/covid-query-0.1.0.jar test2:/home/steve/light-chain/light-config-test/test2/hybrid-query/service
+scp covid-query/target/covid-query-0.1.0.jar test3:/home/steve/light-chain/light-config-test/test3/hybrid-query/service
+scp covid-command/target/covid-command-0.1.0.jar test3:/home/steve/light-chain/light-config-test/test3/hybrid-command/service
