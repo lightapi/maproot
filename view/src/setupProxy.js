b79a2577886d70b1754575948191abf6c70ea875
Steve Hu <stevehu@gmail.com>
2020-06-20 19:38:54 -0400

    fixes #97 update view proxy to point to local.lightapi.net on test3 router

--- view/src/setupProxy.js
+++ view/src/setupProxy.js
@@ -8,15 +8,15 @@ module.exports = function(app) {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
   app.use(cors(corsOptions));
-  app.use(proxy('/portal/command', { target: 'https://dev.lightapi.net', secure: false }));
-  app.use(proxy('/portal/query', { target: 'https://dev.lightapi.net', secure: false }));
-  app.use(proxy('/oauth2/client', { target: 'https://dev.lightapi.net', secure: false }));
-  app.use(proxy('/oauth2/code', { target: 'https://dev.lightapi.net', secure: false }));
-  app.use(proxy('/oauth2/key', { target: 'https://dev.lightapi.net', secure: false }));
-  app.use(proxy('/oauth2/refresh_token', { target: 'https://dev.lightapi.net', secure: false }));
-  app.use(proxy('/oauth2/service', { target: 'https://dev.lightapi.net', secure: false }));
-  app.use(proxy('/oauth2/token', { target: 'https://dev.lightapi.net', secure: false }));
-  app.use(proxy('/oauth2/user', { target: 'https://dev.lightapi.net', secure: false }));
-  app.use(proxy('/authorization', { target: 'https://devsignin.lightapi.net', secure: false }));
-  app.use(proxy('/logout', { target: 'https://devsignin.lightapi.net', secure: false }));
+  app.use(proxy('/portal/command', { target: 'https://local.lightapi.net', secure: false }));
+  app.use(proxy('/portal/query', { target: 'https://local.lightapi.net', secure: false }));
+  app.use(proxy('/oauth2/client', { target: 'https://local.lightapi.net', secure: false }));
+  app.use(proxy('/oauth2/code', { target: 'https://local.lightapi.net', secure: false }));
+  app.use(proxy('/oauth2/key', { target: 'https://local.lightapi.net', secure: false }));
+  app.use(proxy('/oauth2/refresh_token', { target: 'https://local.lightapi.net', secure: false }));
+  app.use(proxy('/oauth2/service', { target: 'https://local.lightapi.net', secure: false }));
+  app.use(proxy('/oauth2/token', { target: 'https://local.lightapi.net', secure: false }));
+  app.use(proxy('/oauth2/user', { target: 'https://local.lightapi.net', secure: false }));
+  app.use(proxy('/authorization', { target: 'https://local.lightapi.net', secure: false }));
+  app.use(proxy('/logout', { target: 'https://local.lightapi.net', secure: false }));
 };
